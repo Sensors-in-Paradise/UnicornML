8be032278d18e50071715e94e2383652d4583911
Alexander <alexander.wischmann@student.hpi.de>
2022-06-17 17:35:41 +0200

    commit all crap

--- src/oh.py
+++ src/oh.py
@@ -20,11 +20,11 @@ confusion_y_test_pred = np.append(confusion_y_test_pred, y_test_pred, axis=0)
 confusion_y_test_pred = np.append(confusion_y_test_pred, y_test_pred, axis=0)
 
 #print(type(y_test_pred))
-print(confusion_y_test_pred)
+print(len(y_test_pred))
 
-calc_metric = lambda v1, v2, v3: (860024*v1 + 952351*v2 + 858291*v3) / (860024+952351+858291)
-s = calc_metric(0.39279, 0.6092, 0.72512)
-s_p = calc_metric(0.40697045134754845, 0.6560398170567662, 0.7153970826580227)
-p = calc_metric(0.31442796839484793, 0.30669895076674736, 0.34683954619124796)
+# calc_metric = lambda v1, v2, v3: (860024*v1 + 952351*v2 + 858291*v3) / (860024+952351+858291)
+# s = calc_metric(0.39279, 0.6092, 0.72512)
+# s_p = calc_metric(0.40697045134754845, 0.6560398170567662, 0.7153970826580227)
+# p = calc_metric(0.31442796839484793, 0.30669895076674736, 0.34683954619124796)
 
-print(s, s_p, p)
+# print(s, s_p, p)
